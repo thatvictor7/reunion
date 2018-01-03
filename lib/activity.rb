1259386ee8164d2e8bacfa747635bd2886cef8e1
thatvictor7 <montoya.victor7@gmail.com>
2018-01-03 11:01:38 -0700

    first submit

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,20 @@
 class Activity
+  attr_reader :name
+  def initialize(name)
+    @name   = name
+    @number = []
+  end
+
+  def participant
+    @name[:participant]
+  end
+
+  def spent
+    @name[:spent]
+  end
+
+  def add
+    @number << @name[:participant]
+  end
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -2,6 +2,42 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/activity'
+require 'pry'
 
 class ActivityTest < Minitest::Test
+
+  # def test_it_exists
+  #   activity = Activity.new
+  #   assert_instance_of Activity, activity
+  # end
+
+  def test_it_activity_has_name
+    activity = Activity.new("hiking")
+    assert_equal "hiking", activity.name
+  end
+
+  def test_different_activity_name
+    activity = Activity.new("volleyball")
+    assert_equal "volleyball", activity.name
+  end
+
+  def test_a_participant
+    person = Activity.new({participant: "Katt", spent: 30 })
+    assert_equal "Katt", person.participant
+  end
+
+  def test_amount_spent_individually_on_activity
+    person = Activity.new({participant: "Katt", spent: 30 })
+    assert_equal 30, person.spent
+  end
+
+  def test_adding_participants_to_activities
+    activity = Activity.new("volleyball")
+    person = Activity.new({participant: "Katt", spent: 30 })
+    assert_equal ["Katt"], person.add
+  end
+
+  def test_adding_multiple_participants
+
+  end
 end
